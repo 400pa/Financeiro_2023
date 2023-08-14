288a77aa249d69decffb241859ad4dd398e67de0
Paulo Lacerda <contatopaulolacerda@gmail.com>
2023-08-13 22:04:26 -0300

    Created Entities

---
+++ Entities/Notificações/Notifica.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Notificações
+{
+
+
+    public class Notifica
+    {
+
+        public Notifica()
+        {
+            notificações = new List<Notifica>();
+        }
+
+        public string NomePropriedade { get; set; }
+
+        public string mensagem { get; set; }
+
+        public List<Notifica> notificações;
+
+
+        public bool ValidarPropriedadeString( string valor, string nomePropriedade)
+        {
+            if (string.IsNullOrWhiteSpace(valor) || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificações.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+                return false;
+            }
+            
+            return true;
+        }
+
+
+        public bool ValidarPropriedadeInt(int Valor, string nomePropriedade) 
+        {
+            if (Valor < 1 || string.IsNullOrWhiteSpace(nomePropriedade))
+            {
+                notificações.Add(new Notifica
+                {
+                    mensagem = "Campo Obrigatório",
+                    NomePropriedade = nomePropriedade,
+                });
+                return false;
+            }
+            return true;
+        
+        }
+    }
+}
+
